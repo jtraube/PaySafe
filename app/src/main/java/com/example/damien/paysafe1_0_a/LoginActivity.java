508c2be2b6ff10cd03b9794ef6725ff098eda5c9
dhobday24 <damienhobday@gmail.com>
2015-11-12 13:52:36 -0500

    Added temporary login bypass button. Added dashboard activity with transaction button that will be linked to the encryption script.

--- app/src/main/java/com/example/damien/paysafe1_0_a/LoginActivity.java
+++ app/src/main/java/com/example/damien/paysafe1_0_a/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.damien.paysafe1_0_a;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -92,6 +93,14 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
+
+        Button bypass_login = (Button) findViewById(R.id.bypass_login);
+        bypass_login.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, Dashboard.class));
+            }
+        });
     }
 
     private void populateAutoComplete() {
